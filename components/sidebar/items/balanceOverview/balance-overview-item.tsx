62bfbf5bae37c512fc6120e8c8ca9c04a732e70b
chielbl <chielbleyenbergh@gmail.com>
2024-07-16 09:42:23 +0200

    feat(add search): added search in balances overview

--- components/sidebar/items/balanceOverview/balance-overview-item.tsx
+++ components/sidebar/items/balanceOverview/balance-overview-item.tsx
@@ -8,11 +8,18 @@ import {
 import { IconCoins } from "@tabler/icons-react"
 import { SIDEBAR_ICON_SIZE } from "../../sidebar-switcher"
 import { Button } from "@/components/ui/button"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { ChatbotUIContext } from "@/context/context"
+import { SidebarSearch } from "../../sidebar-search"
 
 const BalanceOverviewItem = () => {
   const { balances } = useContext(ChatbotUIContext)
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const filteredData =
+    balances.filter(b =>
+      b.name.toLowerCase().includes(searchTerm.toLowerCase())
+    ) || balances
 
   return (
     <Sheet>
@@ -26,18 +33,29 @@ const BalanceOverviewItem = () => {
           <SheetHeader>
             <SheetTitle>Balance Overview</SheetTitle>
           </SheetHeader>
-          <div className="flex flex-col gap-1 mt-4">
-            {balances.length === 0 ? (
+
+          <div className="flex flex-col gap-4">
+            {balances.length > 0 && (
+              <SidebarSearch
+                contentType={"Balances" as any}
+                searchTerm={searchTerm}
+                setSearchTerm={setSearchTerm}
+              />
+            )}
+
+            {filteredData.length === 0 ? (
               <div className=" text-center text-muted-foreground p-8 text-lg italic">
                 No balances.
               </div>
             ) : (
-              balances.map(b => (
-                <div className="flex justify-between">
-                  <p>{b.name}</p>
-                  <p>$ {b.balance.toFixed(2)}</p>
-                </div>
-              ))
+              <div className="flex flex-col gap-1">
+                {filteredData.map(b => (
+                  <div className="flex justify-between">
+                    <p>{b.name}</p>
+                    <p>$ {b.balance.toFixed(2)}</p>
+                  </div>
+                ))}
+              </div>
             )}
           </div>
         </div>
