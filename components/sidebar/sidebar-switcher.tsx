50c41631ef9119ee7c2d80074450909fa5f6f807
kobeDehandschutter <kobe.dehandschutter@gmail.com>
2024-09-05 13:17:58 +0200

    remove really small original logo

--- components/sidebar/sidebar-switcher.tsx
+++ components/sidebar/sidebar-switcher.tsx
@@ -51,7 +51,6 @@ export const SidebarSwitcher: FC<SidebarSwitcherProps> = ({
       <TabsList className="bg-primaryEuricom-900 grid h-[480px] grid-rows-9">
         <div className=" inline-flex items-center justify-center whitespace-nowrap px-3 py-1.5 text-sm">
           <img src="/euricom_logo_small.png" className="h-6 w-6" />
-          <IconMessageChatbot />
         </div>
 
         <SidebarSwitchItem
